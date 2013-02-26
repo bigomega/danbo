271bb7ce2bf2354c7fbc30872b6f67a184b02d0f
Bharath <bharathraja@live.com>
2013-02-27 02:20:03 +0530

    update server: wiki data parse - take links

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, request, Response
 from jinja2 import Environment, PackageLoader
 from urllib import urlopen
 from bs4 import BeautifulSoup
-import json, socket, urllib2
+import json, socket, urllib2, re
 
 app = Flask(__name__)
 env = Environment(loader=PackageLoader('server', 'templates'))
@@ -42,41 +42,43 @@ def local(key):
 	import time
 	import urllib, urllib2
 	import xml.etree.cElementTree as etree
-
+	#
 	title = key or "data_mining"
 	# prepare request
 	maxattempts = 5 # how many times to try the request before giving up
 	maxlag = 5 # seconds http://www.mediawiki.org/wiki/Manual:Maxlag_parameter
-	params = dict(action="query", format="xml", maxlag=maxlag,
-	              prop="revisions", rvprop="content", rvsection=0,
-	              titles=title)
-	request = urllib2.Request(
-	    "http://en.wikipedia.org/w/api.php?" + urllib.urlencode(params), 
-	    headers={"User-Agent": "WikiDownloader/1.2",
-	             "Referer": "http://stackoverflow.com/q/8044814"})
+	params = dict(action="query", format="xml", maxlag=maxlag, prop="revisions", rvprop="content", rvsection=0, titles=title)
+	request = urllib2.Request("http://en.wikipedia.org/w/api.php?" + urllib.urlencode(params), headers={"User-Agent": "WikiDownloader/1.2","Referer": "http://stackoverflow.com/q/8044814"})
 	# make request
 	for _ in range(maxattempts):
-	    response = urllib2.urlopen(request)
-	    if response.headers.get('MediaWiki-API-Error') == 'maxlag':
-	        t = response.headers.get('Retry-After', 5)
-	        print "retrying in %s seconds" % (t,)
-	        time.sleep(float(t))
-	    else:
-	        break # ready to read
+		response = urllib2.urlopen(request)
+		if response.headers.get('MediaWiki-API-Error') == 'maxlag':
+			t = response.headers.get('Retry-After', 5)
+			print "retrying in %s seconds" % (t,)
+			time.sleep(float(t))
+		else:
+			break # ready to read
 	else: # exhausted all attempts
-	    sys.exit(1)
+		sys.exit(1)
 	# download & parse xml 
 	tree = etree.parse(response)
-	
+	#
 	# find rev data 
 	rev_data = tree.findtext('.//rev')
 	if not rev_data:
-	    print 'MediaWiki-API-Error:', response.headers.get('MediaWiki-API-Error')
-	    tree.write(sys.stdout)
-	    print
-	    sys.exit(1)
+		print 'MediaWiki-API-Error:', response.headers.get('MediaWiki-API-Error')
+		tree.write(sys.stdout)
+		print
+		sys.exit(1)
 	
+	str(u[5]).split('|')[-1].replace(r'&nbsp',' ')
 	return rev_data
-	
+
 if __name__ == "__main__":
 	app.run(debug='true')
+	key="chennai"|key
+	data =local(key)
+	links = re.findall(r'\[\[[^\]\]]*\]\]', data, re.M|re.I) #gets the links
+	for link in links:
+		link = re.findall(r'[^\[].*[^\]]', link, re.M|re.I)[0].split('|')[-1].replace(r'&nbsp',' ')
+		print link
